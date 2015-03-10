de0d81cd41bf95e22f4e1a81770c66a139a082ad
fjaeger <fabianjaeger@gmx.net>
2015-03-10 18:00:36 +0100

    - reordered pixel formats

--- typedef.h
+++ typedef.h
@@ -21,19 +21,25 @@
 
 typedef enum {
    YUVC_UnknownPixelFormat                        =  0,
-   YUVC_32RGBAPixelFormat                         =  1, // k32RGBAPixelFormat='RGBA', FourCC 'RGBA'
-   YUVC_24RGBPixelFormat                          =  2, // k24RGBPixelFormat=0x00000018
-   YUVC_24BGRPixelFormat                          =  3, // k24BGRPixelFormat='BG24'
-   YUVC_411YpCbCr8PlanarPixelFormat               =  4,
-   YUVC_420YpCbCr8PlanarPixelFormat               =  5, // FourCC 'i420'
-   YUVC_422YpCbCr8PlanarPixelFormat               =  6,
-   YUVC_UYVY422PixelFormat                        =  7, // kUYVY422PixelFormat='UYVY', k422YpCbCr8CodecType='2vuy', FourCC 'uyvy'
-   YUVC_422YpCbCr10PixelFormat                    =  8, // k422YpCbCr10CodecType='v210', FourCC 'v210'
+   YUVC_GBR12in16LEPlanarPixelFormat              =  1,
+   YUVC_32RGBAPixelFormat                         =  2, // k32RGBAPixelFormat='RGBA', FourCC 'RGBA'
+   YUVC_24RGBPixelFormat                          =  3, // k24RGBPixelFormat=0x00000018
+   YUVC_24BGRPixelFormat                          =  4, // k24BGRPixelFormat='BG24'
+   YUVC_444YpCbCr16LEPlanarPixelFormat            =  5,
+   YUVC_444YpCbCr16BEPlanarPixelFormat            =  6,
+   YUVC_444YpCbCr12LEPlanarPixelFormat            =  7,
+   YUVC_444YpCbCr12BEPlanarPixelFormat            =  8,
    YUVC_444YpCbCr8PlanarPixelFormat               =  9,
-   YUVC_444YpCbCr12LEPlanarPixelFormat            = 10,
-   YUVC_444YpCbCr12BEPlanarPixelFormat            = 11,
-   YUVC_444YpCbCr16LEPlanarPixelFormat            = 12,
-   YUVC_444YpCbCr16BEPlanarPixelFormat            = 13,
+   YUVC_444YpCrCb8PlanarPixelFormat               = 10,
+   YUVC_422YpCbCr8PlanarPixelFormat               = 11,
+   YUVC_422YpCrCb8PlanarPixelFormat               = 12, // 'YV16',
+   YUVC_UYVY422PixelFormat                        = 13, // kUYVY422PixelFormat='UYVY', k422YpCbCr8CodecType='2vuy', FourCC 'uyvy'
+   YUVC_422YpCbCr10PixelFormat                    = 14, // k422YpCbCr10CodecType='v210', FourCC 'v210'
+   YUVC_UYVY422YpCbCr10PixelFormat                = 15, // found in VQEG files
+   YUVC_420YpCbCr10LEPlanarPixelFormat            = 16,   
+   YUVC_420YpCbCr8PlanarPixelFormat               = 17, // FourCC 'i420'
+   YUVC_411YpCbCr8PlanarPixelFormat               = 18,
+   YUVC_8GrayPixelFormat                          = 19, // FourCC 'y800'
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
    YUVC_444YpCbCr12NativePlanarPixelFormat        = YUVC_444YpCbCr12LEPlanarPixelFormat,
    YUVC_444YpCbCr12SwappedPlanarPixelFormat       = YUVC_444YpCbCr12BEPlanarPixelFormat,
@@ -45,12 +51,6 @@ typedef enum {
    YUVC_444YpCbCr16NativePlanarPixelFormat        = YUVC_444YpCbCr16BEPlanarPixelFormat,
    YUVC_444YpCbCr16SwappedPlanarPixelFormat       = YUVC_444YpCbCr16LEPlanarPixelFormat,
 #endif
-   YUVC_8GrayPixelFormat                          = 14, // FourCC 'y800'
-   YUVC_GBR12in16LEPlanarPixelFormat              = 15,
-   YUVC_420YpCbCr10LEPlanarPixelFormat            = 16,
-   YUVC_422YpCrCb8PlanarPixelFormat               = 17, // 'YV16',
-   YUVC_444YpCrCb8PlanarPixelFormat               = 18,
-   YUVC_UYVY422YpCbCr10PixelFormat                = 19  // found in VQEG files
 } YUVCPixelFormatType;
 
 typedef enum {
