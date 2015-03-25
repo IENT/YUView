f8ac4ae055e05f72548e8bcdbad4644c536018eb
Christian <christian.feldmann@gmx.de>
2015-03-25 11:46:14 +0100

    Bugfix: Dragging external items over the emtpy playlist would crash the application. (draggedItems list is empty in this case)

--- playlisttreewidget.cpp
+++ playlisttreewidget.cpp
@@ -53,23 +53,26 @@ PlaylistItem* PlaylistTreeWidget::getDropTarget(QPoint pos)
 void PlaylistTreeWidget::dragMoveEvent(QDragMoveEvent* event)
 {
     PlaylistItem* dropTarget = getDropTarget(event->pos());
-    QList<QTreeWidgetItem*> draggedItems = selectedItems();
-    PlaylistItem* draggedItem = dynamic_cast<PlaylistItem*>(draggedItems[0]);
-
-    // handle video items as target
-    if( dropTarget && dropTarget->itemType() == VideoItemType && (dropTarget->childCount() != 0 || draggedItems.count() != 1 || draggedItem->itemType() != StatisticsItemType ))
+    if (dropTarget)
     {
-        // no valid drop
-        event->ignore();
-        return;
-    }
+        QList<QTreeWidgetItem*> draggedItems = selectedItems();
+        PlaylistItem* draggedItem = dynamic_cast<PlaylistItem*>(draggedItems[0]);
 
-    // handle diff items as target
-    if( dropTarget && dropTarget->itemType() == DifferenceItemType && (dropTarget->childCount() >= 2 || draggedItems.count() > 2 ))
-    {
-        // no valid drop
-        event->ignore();
-        return;
+        // handle video items as target
+        if( dropTarget->itemType() == VideoItemType && (dropTarget->childCount() != 0 || draggedItems.count() != 1 || draggedItem->itemType() != StatisticsItemType ))
+        {
+            // no valid drop
+            event->ignore();
+            return;
+        }
+
+        // handle diff items as target
+        if( dropTarget->itemType() == DifferenceItemType && (dropTarget->childCount() >= 2 || draggedItems.count() > 2 ))
+        {
+            // no valid drop
+            event->ignore();
+            return;
+        }
     }
 
     QTreeWidget::dragMoveEvent(event);
