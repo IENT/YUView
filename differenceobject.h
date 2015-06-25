799025a9e96b4b12f70ec2aea52835b1021c09ae
Max Blaeser <max.blaeser@rwth-aachen.de>
2015-06-25 14:02:16 +0200

    - this is just a test commit to trigger buildbot, please ignore
    Signed-off-by: Max Blaeser <max.blaeser@rwth-aachen.de>

--- differenceobject.h
+++ differenceobject.h
@@ -36,6 +36,7 @@ public:
     void setInternalScaleFactor(int) {}    // no internal scaling
     void refreshDisplayImage()  { p_frameObjects[0]->clearCurrentCache();p_frameObjects[1]->clearCurrentCache();loadImage(p_lastIdx);}
     int numFrames();
+    void markDifferences(); // Todo
 
 private:
     FrameObject* p_frameObjects[2];
