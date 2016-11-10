05fc15d5cdafc21be3d90006308947b6bf12b7b4
Kuba Ober <kuba@mareimbrium.org>
2016-11-10 13:15:04 -0500

    Hold playlistItem::propertiesWidget and playlistItemRawFile::video via smart pointer.

--- source/playlistItemStatisticsFile.cpp
+++ source/playlistItemStatisticsFile.cpp
@@ -544,18 +544,16 @@ void playlistItemStatisticsFile::timerEvent(QTimerEvent * event)
 
 void playlistItemStatisticsFile::createPropertiesWidget()
 {
-  // Absolutely always only call this once// 
-  assert (propertiesWidget == NULL);
+  // Absolutely always only call this once
+  assert(!propertiesWidget);
   
   // Create a new widget and populate it with controls
-  propertiesWidget = new QWidget;
-  if (propertiesWidget->objectName().isEmpty())
-    propertiesWidget->setObjectName(QStringLiteral("playlistItemStatisticsFile"));
+  preparePropertiesWidget(QStringLiteral("playlistItemStatisticsFile"));
 
   // On the top level everything is layout vertically
-  QVBoxLayout *vAllLaout = new QVBoxLayout(propertiesWidget);
+  QVBoxLayout *vAllLaout = new QVBoxLayout(propertiesWidget.data());
 
-  QFrame *line = new QFrame(propertiesWidget);
+  QFrame *line = new QFrame(propertiesWidget.data());
   line->setObjectName(QStringLiteral("lineOne"));
   line->setFrameShape(QFrame::HLine);
   line->setFrameShadow(QFrame::Sunken);
@@ -566,9 +564,6 @@ void playlistItemStatisticsFile::createPropertiesWidget()
 
   // Do not add any stretchers at the bottom because the statistics handler controls will
   // expand to take up as much space as there is available  
-
-  // Set the layout and add widget
-  propertiesWidget->setLayout( vAllLaout );
 }
 
 void playlistItemStatisticsFile::savePlaylist(QDomElement &root, QDir playlistDir)
