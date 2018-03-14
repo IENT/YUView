66afac15b1d5cde871f22b7627f447189885bac4
Max Bläser <blaeser@ient.rwth-aachen.de>
2018-03-14 10:28:56 +0100

    UTF-8 shall rule the world

--- source/singleInstanceHandler.h
+++ source/singleInstanceHandler.h
@@ -1,6 +1,6 @@
 /*  This file is part of YUView - The YUV player with advanced analytics toolset
 *   <https://github.com/IENT/YUView>
-*   Copyright (C) 2015  Institut für Nachrichtentechnik, RWTH Aachen University, GERMANY
+*   Copyright (C) 2015  Institut fÃ¼r Nachrichtentechnik, RWTH Aachen University, GERMANY
 *
 *   This program is free software; you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
