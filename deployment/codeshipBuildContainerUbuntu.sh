8b89b5aa9bf04139c7f1c7c3f965840f02789e39
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-01 17:38:49 +0200

    More fixes

--- deployment/codeshipBuildContainerUbuntu.sh
+++ deployment/codeshipBuildContainerUbuntu.sh
@@ -9,14 +9,15 @@ apt-get install -yq apt-utils
 apt-get update  -yq
 apt-get dist-upgrade -yq
 
-apt-get install -yq build-essential git
+apt-get install -yq build-essential git software-properties-common
 
 if [ "$LINUX_SHORT_NAME" == "ubuntu16.04" ]
+then
   add-apt-repository ppa:beineri/opt-qt-5.12.0-bionic -y
   apt-get update -qq
   apt-get install -qq qt512base qt512charts-no-lgpl
   source /opt/qt512/bin/qt512-env.sh
-then
+else
   apt-get install -yq qt5-default \
                       libqt5charts5 \
                       libqt5charts5-dev
