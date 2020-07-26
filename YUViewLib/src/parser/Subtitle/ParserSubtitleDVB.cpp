0c614b2cc8f6d5155067feb10a761424479fda16
Christian Feldmann <christian.feldmann@bitmovin.com>
2020-07-26 23:57:04 +0200

    Another CI build fix

--- YUViewLib/src/parser/Subtitle/ParserSubtitleDVB.cpp
+++ YUViewLib/src/parser/Subtitle/ParserSubtitleDVB.cpp
@@ -30,7 +30,7 @@
 *   along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "parserSubtitleDVB.h"
+#include "ParserSubtitleDVB.h"
 
 #include "parser/common/ParserMacros.h"
 #include "parser/common/ReaderHelper.h"
