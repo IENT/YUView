904c6cda7e6202b50f77e7cbbfe568694df1ed24
Christian Feldmann <christian.feldmann@gmx.de>
2021-09-27 23:57:49 +0200

    Fix for unit tests

--- YUViewUnitTest/video/rgbPixelFormatTest.cpp
+++ YUViewUnitTest/video/rgbPixelFormatTest.cpp
@@ -2,38 +2,38 @@
 
 #include <video/rgbPixelFormat.h>
 
+using namespace RGB_Internals;
+
 class rgbPixelFormatTest : public QObject
 {
   Q_OBJECT
 
 public:
-  rgbPixelFormatTest() {};
-  ~rgbPixelFormatTest() {};
+  rgbPixelFormatTest(){};
+  ~rgbPixelFormatTest(){};
 
 private slots:
   void testFormatFromToString();
   void testInvalidFormats();
 };
 
-QList<RGB_Internals::rgbPixelFormat> getAllFormats()
+QList<rgbPixelFormat> getAllFormats()
 {
-  QList<RGB_Internals::rgbPixelFormat> allFormats;
+  QList<rgbPixelFormat> allFormats;
 
   for (int bitsPerPixel = 8; bitsPerPixel <= 16; bitsPerPixel++)
   {
     for (auto planar : {false, true})
     {
       // No alpha
-      int positions[] = {0, 1, 2};
-      do {
-        allFormats.append(RGB_Internals::rgbPixelFormat(bitsPerPixel, planar, positions[0], positions[1], positions[2]));
-      } while (std::next_permutation(positions,positions+3));
-
+      for (auto channelOrder : ChannelOrderMapper.getEnums())
+        allFormats.append(rgbPixelFormat(bitsPerPixel, planar, channelOrder));
       // With alpha
-      int positionsA[] = {0, 1, 2, 3};
-      do {
-        allFormats.append(RGB_Internals::rgbPixelFormat(bitsPerPixel, planar, positionsA[0], positionsA[1], positionsA[2], positionsA[3]));
-      } while (std::next_permutation(positionsA,positionsA+4));
+      for (auto channelOrder : ChannelOrderMapper.getEnums())
+      {
+        allFormats.append(rgbPixelFormat(bitsPerPixel, planar, channelOrder, true));
+        allFormats.append(rgbPixelFormat(bitsPerPixel, planar, channelOrder, true, true));
+      }
     }
   }
 
@@ -50,28 +50,27 @@ void rgbPixelFormatTest::testFormatFromToString()
     {
       QFAIL("Name empty");
     }
-    auto fmtNew = RGB_Internals::rgbPixelFormat(name);
+    auto fmtNew = rgbPixelFormat(name);
     if (fmt != fmtNew)
     {
       auto errorStr = "Comparison failed. Names: " + name;
       QFAIL(errorStr.c_str());
     }
-    if (fmt.posR != fmtNew.posR ||
-        fmt.posG != fmtNew.posG ||
-        fmt.posB != fmtNew.posB ||
-        fmt.posA != fmtNew.posA ||
-        fmt.bitsPerValue != fmtNew.bitsPerValue ||
-        fmt.planar != fmtNew.planar)
+    if (fmt.getComponentPosition(Channel::Red) != fmtNew.getComponentPosition(Channel::Red) ||
+        fmt.getComponentPosition(Channel::Green) != fmtNew.getComponentPosition(Channel::Green) ||
+        fmt.getComponentPosition(Channel::Blue) != fmtNew.getComponentPosition(Channel::Blue) ||
+        fmt.getComponentPosition(Channel::Alpha) != fmtNew.getComponentPosition(Channel::Alpha) ||
+        fmt.getBitsPerSample() != fmtNew.getBitsPerSample() || fmt.isPlanar() != fmtNew.isPlanar())
     {
       auto errorStr = "Comparison of parameters failed. Names: " + name;
       QFAIL(errorStr.c_str());
     }
-    if (fmt.posA != -1 && (fmt.nrChannels() != 4 || !fmt.hasAlphaChannel()))
+    if (fmt.hasAlphaChannel() && (fmt.nrChannels() != 4 || !fmt.hasAlphaChannel()))
     {
       auto errorStr = "Alpha channel indicated wrong - " + name;
       QFAIL(errorStr.c_str());
     }
-    if (fmt.posA == -1 && (fmt.nrChannels() != 3 || fmt.hasAlphaChannel()))
+    if (!fmt.hasAlphaChannel() && (fmt.nrChannels() != 3 || fmt.hasAlphaChannel()))
     {
       auto errorStr = "Alpha channel indicated wrong - %1" + name;
       QFAIL(errorStr.c_str());
@@ -81,21 +80,12 @@ void rgbPixelFormatTest::testFormatFromToString()
 
 void rgbPixelFormatTest::testInvalidFormats()
 {
-  QList<RGB_Internals::rgbPixelFormat> invalidFormats;
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(0, false));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(1, false));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(7, false));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(17, false));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(200, false));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 0, 0, 0));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 0, 1, 1));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 1, 2, 2));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 1, 2, 3));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 0, 1, 4));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 8, 0, 1));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 0, 1, 2, 1));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 0, 1, 2, 4));
-  invalidFormats.append(RGB_Internals::rgbPixelFormat(8, false, 0, 22, 2, 3));
+  QList<rgbPixelFormat> invalidFormats;
+  invalidFormats.append(rgbPixelFormat(0, false, ChannelOrder::RGB));
+  invalidFormats.append(rgbPixelFormat(1, false, ChannelOrder::RGB));
+  invalidFormats.append(rgbPixelFormat(7, false, ChannelOrder::RGB));
+  invalidFormats.append(rgbPixelFormat(17, false, ChannelOrder::RGB));
+  invalidFormats.append(rgbPixelFormat(200, false, ChannelOrder::RGB));
 
   for (auto fmt : invalidFormats)
     QVERIFY(!fmt.isValid());
