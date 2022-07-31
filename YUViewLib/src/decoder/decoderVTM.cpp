fd5efefb680eae6204a16f63314438985b488dc3
Christian Feldmann <christian.feldmann@bitmovin.com>
2022-07-31 21:51:35 +0200

    Fixed vtm decoder

--- YUViewLib/src/decoder/decoderVTM.cpp
+++ YUViewLib/src/decoder/decoderVTM.cpp
@@ -347,6 +347,99 @@ bool decoderVTM::pushData(QByteArray &data)
   return !bNewPicture;
 }
 
+stats::StatisticsTypes decoderVTM::getStatisticsTypes() const
+{
+  // Ask the decoder how many internals types there are
+  // unsigned int nrTypes = this->lib.libVTMDec_get_internal_type_number();
+
+  // for (unsigned int i = 0; i < nrTypes; i++)
+  //{
+  //  QString name = this->lib.libVTMDec_get_internal_type_name(i);
+  //  QString description = this->lib.libVTMDec_get_internal_type_description(i);
+  //  auto statType = this->lib.libVTMDec_get_internal_type(i);
+  //  int max = 0;
+  //  if (statType == libVTMDec_TYPE_RANGE || statType == libVTMDec_TYPE_RANGE_ZEROCENTER)
+  //  {
+  //    unsigned int uMax = this->lib.libVTMDec_get_internal_type_max(i);
+  //    max = (uMax > INT_MAX) ? INT_MAX : uMax;
+  //  }
+
+  //  if (statType == libVTMDec_TYPE_FLAG)
+  //  {
+  //    StatisticsType flag(i, name, "jet", 0, 1);
+  //    flag.description = description;
+  //    statSource.addStatType(flag);
+  //  }
+  //  else if (statType == libVTMDec_TYPE_RANGE)
+  //  {
+  //    StatisticsType range(i, name, "jet", 0, max);
+  //    range.description = description;
+  //    statSource.addStatType(range);
+  //  }
+  //  else if (statType == libVTMDec_TYPE_RANGE_ZEROCENTER)
+  //  {
+  //    StatisticsType rangeZero(i, name, "col3_bblg", -max, max);
+  //    rangeZero.description = description;
+  //    statSource.addStatType(rangeZero);
+  //  }
+  //  else if (statType == libVTMDec_TYPE_VECTOR)
+  //  {
+  //    unsigned int scale = this->lib.libVTMDec_get_internal_type_vector_scaling(i);
+  //    StatisticsType vec(i, name, scale);
+  //    vec.description = description;
+  //    statSource.addStatType(vec);
+  //  }
+  //  else if (statType == libVTMDec_TYPE_INTRA_DIR)
+  //  {
+  //    StatisticsType intraDir(i, name, "jet", 0, 34);
+  //    intraDir.description = description;
+  //    intraDir.hasVectorData = true;
+  //    intraDir.renderVectorData = true;
+  //    intraDir.vectorScale = 32;
+  //    // Don't draw the vector values for the intra dir. They don't have actual meaning.
+  //    intraDir.renderVectorDataValues = false;
+  //    intraDir.valMap.insert(0, "INTRA_PLANAR");
+  //    intraDir.valMap.insert(1, "INTRA_DC");
+  //    intraDir.valMap.insert(2, "INTRA_ANGULAR_2");
+  //    intraDir.valMap.insert(3, "INTRA_ANGULAR_3");
+  //    intraDir.valMap.insert(4, "INTRA_ANGULAR_4");
+  //    intraDir.valMap.insert(5, "INTRA_ANGULAR_5");
+  //    intraDir.valMap.insert(6, "INTRA_ANGULAR_6");
+  //    intraDir.valMap.insert(7, "INTRA_ANGULAR_7");
+  //    intraDir.valMap.insert(8, "INTRA_ANGULAR_8");
+  //    intraDir.valMap.insert(9, "INTRA_ANGULAR_9");
+  //    intraDir.valMap.insert(10, "INTRA_ANGULAR_10");
+  //    intraDir.valMap.insert(11, "INTRA_ANGULAR_11");
+  //    intraDir.valMap.insert(12, "INTRA_ANGULAR_12");
+  //    intraDir.valMap.insert(13, "INTRA_ANGULAR_13");
+  //    intraDir.valMap.insert(14, "INTRA_ANGULAR_14");
+  //    intraDir.valMap.insert(15, "INTRA_ANGULAR_15");
+  //    intraDir.valMap.insert(16, "INTRA_ANGULAR_16");
+  //    intraDir.valMap.insert(17, "INTRA_ANGULAR_17");
+  //    intraDir.valMap.insert(18, "INTRA_ANGULAR_18");
+  //    intraDir.valMap.insert(19, "INTRA_ANGULAR_19");
+  //    intraDir.valMap.insert(20, "INTRA_ANGULAR_20");
+  //    intraDir.valMap.insert(21, "INTRA_ANGULAR_21");
+  //    intraDir.valMap.insert(22, "INTRA_ANGULAR_22");
+  //    intraDir.valMap.insert(23, "INTRA_ANGULAR_23");
+  //    intraDir.valMap.insert(24, "INTRA_ANGULAR_24");
+  //    intraDir.valMap.insert(25, "INTRA_ANGULAR_25");
+  //    intraDir.valMap.insert(26, "INTRA_ANGULAR_26");
+  //    intraDir.valMap.insert(27, "INTRA_ANGULAR_27");
+  //    intraDir.valMap.insert(28, "INTRA_ANGULAR_28");
+  //    intraDir.valMap.insert(29, "INTRA_ANGULAR_29");
+  //    intraDir.valMap.insert(30, "INTRA_ANGULAR_30");
+  //    intraDir.valMap.insert(31, "INTRA_ANGULAR_31");
+  //    intraDir.valMap.insert(32, "INTRA_ANGULAR_32");
+  //    intraDir.valMap.insert(33, "INTRA_ANGULAR_33");
+  //    intraDir.valMap.insert(34, "INTRA_ANGULAR_34");
+  //    statSource.addStatType(intraDir);
+  //  }
+  //}
+
+  return {};
+}
+
 QByteArray decoderVTM::getRawFrameData()
 {
   if (this->currentVTMPic == nullptr)
@@ -369,6 +462,79 @@ QByteArray decoderVTM::getRawFrameData()
   }
 
   return this->currentOutputBuffer;
+}
+
+stats::DataPerTypeMap decoderVTM::getFrameStatisticsData()
+{
+  if (this->currentVTMPic == nullptr)
+    return {};
+  if (this->decoderState != DecoderState::RetrieveFrames)
+  {
+    DEBUG_DECVTM("decoderVTM::getRawFrameData: Wrong decoder state.");
+    return {};
+  }
+
+  if (!this->statisticsEnabled)
+    return {};
+
+  DEBUG_DECVTM("decoderVTM::cacheStatistics ...");
+
+  // // Conversion from intra prediction mode to vector.
+  // // Coordinates are in x,y with the axes going right and down.
+  // static const int vectorTable[35][2] =
+  // {
+  //   {0,0}, {0,0},
+  //   {32, -32},
+  //   {32, -26}, {32, -21}, {32, -17}, { 32, -13}, { 32,  -9}, { 32, -5}, { 32, -2},
+  //   {32,   0},
+  //   {32,   2}, {32,   5}, {32,   9}, { 32,  13}, { 32,  17}, { 32, 21}, { 32, 26},
+  //   {32,  32},
+  //   {26,  32}, {21,  32}, {17,  32}, { 13,  32}, {  9,  32}, {  5, 32}, {  2, 32},
+  //   {0,   32},
+  //   {-2,  32}, {-5,  32}, {-9,  32}, {-13,  32}, {-17,  32}, {-21, 32}, {-26, 32},
+  //   {-32, 32}
+  // };
+
+  //// Get all the statistics
+  //// TODO: Could we only retrieve the statistics that are active/displayed?
+  // unsigned int nrTypes = libVTMDec_get_internal_type_number();
+  // for (unsigned int t = 0; t <= nrTypes; t++)
+  //{
+  //  bool callAgain;
+  //  do
+  //  {
+  //    // Get a pointer to the data values and how many values in this array are valid.
+  //    unsigned int nrValues;
+  //    libVTMDec_BlockValue *stats = libVTMDec_get_internal_info(decoder, img, t, nrValues,
+  //    callAgain);
+
+  //    libVTMDec_InternalsType statType = libVTMDec_get_internal_type(t);
+  //    if (stats != nullptr && nrValues > 0)
+  //    {
+  //      for (unsigned int i = 0; i < nrValues; i++)
+  //      {
+  //        libVTMDec_BlockValue b = stats[i];
+
+  //        if (statType == libVTMDec_TYPE_VECTOR)
+  //          curPOCStats[t].addBlockVector(b.x, b.y, b.w, b.h, b.value, b.value2);
+  //        else
+  //          curPOCStats[t].addBlockValue(b.x, b.y, b.w, b.h, b.value);
+  //        if (statType == libVTMDec_TYPE_INTRA_DIR)
+  //        {
+  //          // Also add the vecotr to draw
+  //          if (b.value >= 0 && b.value < 35)
+  //          {
+  //            int vecX = (float)vectorTable[b.value][0] * b.w / 4;
+  //            int vecY = (float)vectorTable[b.value][1] * b.w / 4;
+  //            curPOCStats[t].addBlockVector(b.x, b.y, b.w, b.h, vecX, vecY);
+  //          }
+  //        }
+  //      }
+  //    }
+  //  } while (callAgain); // Continue until the library returns that there is no more to retrive
+  //}
+
+  return {};
 }
 
 #if SSE_CONVERSION
@@ -459,66 +625,6 @@ void decoderVTM::copyImgToByteArray(libVTMDec_picture *src, QByteArray &dst)
   }
 }
 
-void decoderVTM::cacheStatistics(libVTMDec_picture *)
-{
-  DEBUG_DECVTM("decoderVTM::cacheStatistics ...");
-
-  // // Conversion from intra prediction mode to vector.
-  // // Coordinates are in x,y with the axes going right and down.
-  // static const int vectorTable[35][2] =
-  // {
-  //   {0,0}, {0,0},
-  //   {32, -32},
-  //   {32, -26}, {32, -21}, {32, -17}, { 32, -13}, { 32,  -9}, { 32, -5}, { 32, -2},
-  //   {32,   0},
-  //   {32,   2}, {32,   5}, {32,   9}, { 32,  13}, { 32,  17}, { 32, 21}, { 32, 26},
-  //   {32,  32},
-  //   {26,  32}, {21,  32}, {17,  32}, { 13,  32}, {  9,  32}, {  5, 32}, {  2, 32},
-  //   {0,   32},
-  //   {-2,  32}, {-5,  32}, {-9,  32}, {-13,  32}, {-17,  32}, {-21, 32}, {-26, 32},
-  //   {-32, 32}
-  // };
-
-  //// Get all the statistics
-  //// TODO: Could we only retrieve the statistics that are active/displayed?
-  // unsigned int nrTypes = libVTMDec_get_internal_type_number();
-  // for (unsigned int t = 0; t <= nrTypes; t++)
-  //{
-  //  bool callAgain;
-  //  do
-  //  {
-  //    // Get a pointer to the data values and how many values in this array are valid.
-  //    unsigned int nrValues;
-  //    libVTMDec_BlockValue *stats = libVTMDec_get_internal_info(decoder, img, t, nrValues,
-  //    callAgain);
-
-  //    libVTMDec_InternalsType statType = libVTMDec_get_internal_type(t);
-  //    if (stats != nullptr && nrValues > 0)
-  //    {
-  //      for (unsigned int i = 0; i < nrValues; i++)
-  //      {
-  //        libVTMDec_BlockValue b = stats[i];
-
-  //        if (statType == libVTMDec_TYPE_VECTOR)
-  //          curPOCStats[t].addBlockVector(b.x, b.y, b.w, b.h, b.value, b.value2);
-  //        else
-  //          curPOCStats[t].addBlockValue(b.x, b.y, b.w, b.h, b.value);
-  //        if (statType == libVTMDec_TYPE_INTRA_DIR)
-  //        {
-  //          // Also add the vecotr to draw
-  //          if (b.value >= 0 && b.value < 35)
-  //          {
-  //            int vecX = (float)vectorTable[b.value][0] * b.w / 4;
-  //            int vecY = (float)vectorTable[b.value][1] * b.w / 4;
-  //            curPOCStats[t].addBlockVector(b.x, b.y, b.w, b.h, vecX, vecY);
-  //          }
-  //        }
-  //      }
-  //    }
-  //  } while (callAgain); // Continue until the library returns that there is no more to retrive
-  //}
-}
-
 QString decoderVTM::getDecoderName() const
 {
   return (this->decoderState == DecoderState::Error) ? "VTM" : this->lib.libVTMDec_get_version();
@@ -541,97 +647,6 @@ bool decoderVTM::checkLibraryFile(QString libFilePath, QString &error)
   testDecoder.resolveLibraryFunctionPointers();
   error = testDecoder.decoderErrorString();
   return testDecoder.state() != DecoderState::Error;
-}
-
-void decoderVTM::setStatisticsTypesInStatisticsData()
-{
-  // Ask the decoder how many internals types there are
-  // unsigned int nrTypes = this->lib.libVTMDec_get_internal_type_number();
-
-  // for (unsigned int i = 0; i < nrTypes; i++)
-  //{
-  //  QString name = this->lib.libVTMDec_get_internal_type_name(i);
-  //  QString description = this->lib.libVTMDec_get_internal_type_description(i);
-  //  auto statType = this->lib.libVTMDec_get_internal_type(i);
-  //  int max = 0;
-  //  if (statType == libVTMDec_TYPE_RANGE || statType == libVTMDec_TYPE_RANGE_ZEROCENTER)
-  //  {
-  //    unsigned int uMax = this->lib.libVTMDec_get_internal_type_max(i);
-  //    max = (uMax > INT_MAX) ? INT_MAX : uMax;
-  //  }
-
-  //  if (statType == libVTMDec_TYPE_FLAG)
-  //  {
-  //    StatisticsType flag(i, name, "jet", 0, 1);
-  //    flag.description = description;
-  //    statSource.addStatType(flag);
-  //  }
-  //  else if (statType == libVTMDec_TYPE_RANGE)
-  //  {
-  //    StatisticsType range(i, name, "jet", 0, max);
-  //    range.description = description;
-  //    statSource.addStatType(range);
-  //  }
-  //  else if (statType == libVTMDec_TYPE_RANGE_ZEROCENTER)
-  //  {
-  //    StatisticsType rangeZero(i, name, "col3_bblg", -max, max);
-  //    rangeZero.description = description;
-  //    statSource.addStatType(rangeZero);
-  //  }
-  //  else if (statType == libVTMDec_TYPE_VECTOR)
-  //  {
-  //    unsigned int scale = this->lib.libVTMDec_get_internal_type_vector_scaling(i);
-  //    StatisticsType vec(i, name, scale);
-  //    vec.description = description;
-  //    statSource.addStatType(vec);
-  //  }
-  //  else if (statType == libVTMDec_TYPE_INTRA_DIR)
-  //  {
-  //    StatisticsType intraDir(i, name, "jet", 0, 34);
-  //    intraDir.description = description;
-  //    intraDir.hasVectorData = true;
-  //    intraDir.renderVectorData = true;
-  //    intraDir.vectorScale = 32;
-  //    // Don't draw the vector values for the intra dir. They don't have actual meaning.
-  //    intraDir.renderVectorDataValues = false;
-  //    intraDir.valMap.insert(0, "INTRA_PLANAR");
-  //    intraDir.valMap.insert(1, "INTRA_DC");
-  //    intraDir.valMap.insert(2, "INTRA_ANGULAR_2");
-  //    intraDir.valMap.insert(3, "INTRA_ANGULAR_3");
-  //    intraDir.valMap.insert(4, "INTRA_ANGULAR_4");
-  //    intraDir.valMap.insert(5, "INTRA_ANGULAR_5");
-  //    intraDir.valMap.insert(6, "INTRA_ANGULAR_6");
-  //    intraDir.valMap.insert(7, "INTRA_ANGULAR_7");
-  //    intraDir.valMap.insert(8, "INTRA_ANGULAR_8");
-  //    intraDir.valMap.insert(9, "INTRA_ANGULAR_9");
-  //    intraDir.valMap.insert(10, "INTRA_ANGULAR_10");
-  //    intraDir.valMap.insert(11, "INTRA_ANGULAR_11");
-  //    intraDir.valMap.insert(12, "INTRA_ANGULAR_12");
-  //    intraDir.valMap.insert(13, "INTRA_ANGULAR_13");
-  //    intraDir.valMap.insert(14, "INTRA_ANGULAR_14");
-  //    intraDir.valMap.insert(15, "INTRA_ANGULAR_15");
-  //    intraDir.valMap.insert(16, "INTRA_ANGULAR_16");
-  //    intraDir.valMap.insert(17, "INTRA_ANGULAR_17");
-  //    intraDir.valMap.insert(18, "INTRA_ANGULAR_18");
-  //    intraDir.valMap.insert(19, "INTRA_ANGULAR_19");
-  //    intraDir.valMap.insert(20, "INTRA_ANGULAR_20");
-  //    intraDir.valMap.insert(21, "INTRA_ANGULAR_21");
-  //    intraDir.valMap.insert(22, "INTRA_ANGULAR_22");
-  //    intraDir.valMap.insert(23, "INTRA_ANGULAR_23");
-  //    intraDir.valMap.insert(24, "INTRA_ANGULAR_24");
-  //    intraDir.valMap.insert(25, "INTRA_ANGULAR_25");
-  //    intraDir.valMap.insert(26, "INTRA_ANGULAR_26");
-  //    intraDir.valMap.insert(27, "INTRA_ANGULAR_27");
-  //    intraDir.valMap.insert(28, "INTRA_ANGULAR_28");
-  //    intraDir.valMap.insert(29, "INTRA_ANGULAR_29");
-  //    intraDir.valMap.insert(30, "INTRA_ANGULAR_30");
-  //    intraDir.valMap.insert(31, "INTRA_ANGULAR_31");
-  //    intraDir.valMap.insert(32, "INTRA_ANGULAR_32");
-  //    intraDir.valMap.insert(33, "INTRA_ANGULAR_33");
-  //    intraDir.valMap.insert(34, "INTRA_ANGULAR_34");
-  //    statSource.addStatType(intraDir);
-  //  }
-  //}
 }
 
 } // namespace decoder
