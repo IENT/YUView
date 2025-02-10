665c6ba7398149b95ef51983a23d9bdcdc928fcb
ChristianFeldmann <christian.feldmann@gmx.de>
2025-02-10 21:57:09 +0100

    Modify tests to not write to string. That is unstable if the XML library saves the parameters a bit different. Add remaining loading tests.

--- YUViewUnitTest/statistics/StatisticsTypePlaylistHandlerTest.cpp
+++ YUViewUnitTest/statistics/StatisticsTypePlaylistHandlerTest.cpp
@@ -40,7 +40,10 @@
 namespace stats::test
 {
 
-QString saveItemAndGetXmlAsText(const StatisticsType &statisticsType)
+namespace
+{
+
+QDomElement saveItemAndTryGetAsElement(const StatisticsType &statisticsType)
 {
   QDomDocument document;
 
@@ -48,8 +51,46 @@ QString saveItemAndGetXmlAsText(const StatisticsType &statisticsType)
   StatisticsTypePlaylistHandler::saveToPlaylist(statisticsType, d);
   document.appendChild(d);
 
-  return document.toString();
+  return document.firstChildElement("playlistItemStatisticsFile").firstChild().toElement();
 }
+
+QDomDocument loadAndCheckXMLDocument(const QString &xml)
+{
+  QDomDocument document;
+  EXPECT_TRUE(document.setContent(xml));
+
+  const auto root    = document.documentElement();
+  const auto tagName = root.tagName();
+  EXPECT_EQ(tagName, "playlistItemStatisticsFile");
+
+  return document;
+}
+
+bool anyValueModified(const StatisticsType &type)
+{
+  const auto valueDataOptionsModified =
+      type.valueDataOptions && (type.valueDataOptions->render.wasModified() ||
+                                type.valueDataOptions->scaleToBlockSize.wasModified() ||
+                                type.valueDataOptions->colorMapper.wasModified());
+
+  const auto vectorDataOptionsModified =
+      type.vectorDataOptions &&
+      (type.vectorDataOptions->render.wasModified() ||
+       type.vectorDataOptions->renderDataValues.wasModified() ||
+       type.vectorDataOptions->scaleToZoom.wasModified() ||
+       type.vectorDataOptions->style.wasModified() || type.vectorDataOptions->scale.wasModified() ||
+       type.vectorDataOptions->mapToColor.wasModified() ||
+       type.vectorDataOptions->arrowHead.wasModified());
+
+  const auto gridOptionsModified = type.gridOptions.render.wasModified() || //
+                                   type.gridOptions.style.wasModified() ||  //
+                                   type.gridOptions.scaleToZoom.wasModified();
+
+  return type.render.wasModified() || type.alphaFactor.wasModified() || valueDataOptionsModified ||
+         vectorDataOptionsModified || gridOptionsModified;
+}
+
+} // namespace
 
 TEST(StatisticsTypePlaylistHandlerTest,
      SaveStatisticsType_NoOptionsModified_ShouldNotAddTheTypeAtAll)
@@ -62,7 +103,7 @@ TEST(StatisticsTypePlaylistHandlerTest,
               {.render = false, .scaleToBlockSize = true, .colorMapper = colorMapper})
           .build();
 
-  EXPECT_EQ(saveItemAndGetXmlAsText(statisticsType), "<playlistItemStatisticsFile/>\n");
+  EXPECT_TRUE(saveItemAndTryGetAsElement(statisticsType).isNull());
 }
 
 TEST(StatisticsTypePlaylistHandlerTest,
@@ -75,10 +116,12 @@ TEST(StatisticsTypePlaylistHandlerTest,
   statisticsType.render      = true;
   statisticsType.alphaFactor = 98;
 
-  EXPECT_EQ(saveItemAndGetXmlAsText(statisticsType),
-            "<playlistItemStatisticsFile>\n"
-            " <statType1 alphaFactor=\"98\" render=\"1\">SomeTestType</statType1>\n"
-            "</playlistItemStatisticsFile>\n");
+  const auto item = saveItemAndTryGetAsElement(statisticsType);
+  EXPECT_FALSE(item.isNull());
+  EXPECT_EQ(item.tagName(), "statType1");
+  EXPECT_EQ(item.text(), "SomeTestType");
+  EXPECT_EQ(item.attribute("render").toInt(), 1);
+  EXPECT_EQ(item.attribute("alphaFactor").toInt(), 98);
 }
 
 TEST(StatisticsTypePlaylistHandlerTest,
@@ -95,12 +138,15 @@ TEST(StatisticsTypePlaylistHandlerTest,
   statisticsType.valueDataOptions->colorMapper =
       color::ColorMapper({0, 255}, color::PredefinedType::Jet);
 
-  EXPECT_EQ(saveItemAndGetXmlAsText(statisticsType),
-            "<playlistItemStatisticsFile>\n"
-            " <statType1 colorMapperPredefinedType=\"Jet\" colorMapperRange=\"0|255\" "
-            "colorMapperType=\"Predefined\" renderValueData=\"0\" "
-            "scaleValueToBlockSize=\"0\">SomeTestType</statType1>\n" //
-            "</playlistItemStatisticsFile>\n");
+  const auto item = saveItemAndTryGetAsElement(statisticsType);
+  EXPECT_FALSE(item.isNull());
+  EXPECT_EQ(item.tagName(), "statType1");
+  EXPECT_EQ(item.text(), "SomeTestType");
+  EXPECT_EQ(item.attribute("colorMapperPredefinedType"), "Jet");
+  EXPECT_EQ(item.attribute("colorMapperRange"), "0|255");
+  EXPECT_EQ(item.attribute("colorMapperType"), "Predefined");
+  EXPECT_EQ(item.attribute("renderValueData"), "0");
+  EXPECT_EQ(item.attribute("scaleValueToBlockSize"), "0");
 }
 
 TEST(StatisticsTypePlaylistHandlerTest,
@@ -121,14 +167,17 @@ TEST(StatisticsTypePlaylistHandlerTest,
   statisticsType.vectorDataOptions->mapToColor = true;
   statisticsType.vectorDataOptions->arrowHead  = StatisticsType::ArrowHead::circle;
 
-  std::cout << saveItemAndGetXmlAsText(statisticsType).toStdString() << std::endl;
-
-  EXPECT_EQ(saveItemAndGetXmlAsText(statisticsType),
-            "<playlistItemStatisticsFile>\n"
-            " <statType1 mapVectorToColor=\"1\" renderVectorData=\"0\" "
-            "renderVectorDataValues=\"0\" renderarrowHead=\"1\" scaleVectorToZoom=\"1\" "
-            "vectorScale=\"4\" vectorStyle=\"#80164c 2 3\">SomeTestType</statType1>\n"
-            "</playlistItemStatisticsFile>\n");
+  const auto item = saveItemAndTryGetAsElement(statisticsType);
+  EXPECT_FALSE(item.isNull());
+  EXPECT_EQ(item.tagName(), "statType1");
+  EXPECT_EQ(item.text(), "SomeTestType");
+  EXPECT_EQ(item.attribute("mapVectorToColor"), "1");
+  EXPECT_EQ(item.attribute("renderVectorData"), "0");
+  EXPECT_EQ(item.attribute("renderVectorDataValues"), "0");
+  EXPECT_EQ(item.attribute("renderarrowHead"), "1");
+  EXPECT_EQ(item.attribute("scaleVectorToZoom"), "1");
+  EXPECT_EQ(item.attribute("vectorScale"), "4");
+  EXPECT_EQ(item.attribute("vectorStyle"), "#80164c 2 3");
 }
 
 TEST(StatisticsTypePlaylistHandlerTest,
@@ -144,11 +193,154 @@ TEST(StatisticsTypePlaylistHandlerTest,
   statisticsType.gridOptions.style       = LineDrawStyle({Color(12, 15, 240), 1.25, Pattern::Dash});
   statisticsType.gridOptions.scaleToZoom = true;
 
-  EXPECT_EQ(saveItemAndGetXmlAsText(statisticsType),
-            "<playlistItemStatisticsFile>\n"
-            " <statType1 gridStyle=\"#0c0ff0 1.25 1\" renderGrid=\"1\" "
-            "scaleGridToZoom=\"1\">SomeTestType</statType1>\n"
-            "</playlistItemStatisticsFile>\n");
+  const auto item = saveItemAndTryGetAsElement(statisticsType);
+  EXPECT_FALSE(item.isNull());
+  EXPECT_EQ(item.tagName(), "statType1");
+  EXPECT_EQ(item.text(), "SomeTestType");
+  EXPECT_EQ(item.attribute("gridStyle"), "#0c0ff0 1.25 1");
+  EXPECT_EQ(item.attribute("renderGrid"), "1");
+  EXPECT_EQ(item.attribute("scaleGridToZoom"), "1");
+}
+
+TEST(StatisticsTypePlaylistHandlerTest, LoadStatisticsType_EmptyXML_ShouldNotModifyStatisticsType)
+{
+  auto statisticsType = stats::StatisticsTypeBuilder(1, "SomeTestType")
+                            .withValueDataOptions({})
+                            .withVectorDataOptions({})
+                            .build();
+
+  const auto document = loadAndCheckXMLDocument("<playlistItemStatisticsFile/>");
+
+  StatisticsTypePlaylistHandler::tryToLoadFromPlaylist(statisticsType, document.documentElement());
+
+  const auto expectedStatisticsType = stats::StatisticsTypeBuilder(1, "SomeTestType")
+                                          .withValueDataOptions({})
+                                          .withVectorDataOptions({})
+                                          .build();
+  EXPECT_EQ(expectedStatisticsType, statisticsType);
+}
+
+TEST(StatisticsTypePlaylistHandlerTest,
+     LoadStatisticsType_StatisticsTypeNotInXML_ShouldNotModifyStatisticsType)
+{
+  auto statisticsType = stats::StatisticsTypeBuilder(0, "SomeTestType")
+                            .withValueDataOptions({})
+                            .withVectorDataOptions({})
+                            .build();
+
+  const auto document =
+      loadAndCheckXMLDocument("<playlistItemStatisticsFile>\n"
+                              " <statType1 gridStyle=\"#0c0ff0 1.25 1\" renderGrid=\"1\" "
+                              "scaleGridToZoom=\"1\">SomeTestType</statType1>\n"
+                              "</playlistItemStatisticsFile>\n");
+
+  StatisticsTypePlaylistHandler::tryToLoadFromPlaylist(statisticsType, document.documentElement());
+
+  const auto expectedStatisticsType = stats::StatisticsTypeBuilder(0, "SomeTestType")
+                                          .withValueDataOptions({})
+                                          .withVectorDataOptions({})
+                                          .build();
+  EXPECT_EQ(expectedStatisticsType, statisticsType);
+}
+
+TEST(StatisticsTypePlaylistHandlerTest,
+     LoadStatisticsType_RenderAndAlphaFactorInXML_ShouldBeCorrectlySetInStatisticsType)
+{
+  auto statisticsType = stats::StatisticsTypeBuilder(1, "SomeTestType").build();
+
+  const auto document = loadAndCheckXMLDocument(
+      "<playlistItemStatisticsFile>\n"
+      " <statType1 alphaFactor=\"98\" render=\"1\">SomeTestType</statType1>\n"
+      "</playlistItemStatisticsFile>\n");
+
+  StatisticsTypePlaylistHandler::tryToLoadFromPlaylist(statisticsType, document.documentElement());
+
+  EXPECT_TRUE(statisticsType.render);
+  EXPECT_EQ(statisticsType.alphaFactor, 98);
+
+  EXPECT_FALSE(statisticsType.valueDataOptions);
+  EXPECT_FALSE(statisticsType.vectorDataOptions);
+  EXPECT_EQ(statisticsType.gridOptions, StatisticsType::GridOptions());
+
+  EXPECT_FALSE(anyValueModified(statisticsType));
+}
+
+TEST(StatisticsTypePlaylistHandlerTest,
+     LoadStatisticsType_ValueDataOptionsInXML_ShouldBeCorrectlySetInStatisticsType)
+{
+  auto statisticsType = stats::StatisticsTypeBuilder(1, "SomeTestType").build();
+
+  const auto document = loadAndCheckXMLDocument(
+      "<playlistItemStatisticsFile>\n"
+      " <statType1 colorMapperPredefinedType=\"Jet\" colorMapperRange=\"0|255\" "
+      "colorMapperType=\"Predefined\" renderValueData=\"0\" scaleValueToBlockSize=\"0\">"
+      "SomeTestType</statType1>\n"
+      "</playlistItemStatisticsFile>\n");
+
+  StatisticsTypePlaylistHandler::tryToLoadFromPlaylist(statisticsType, document.documentElement());
+
+  EXPECT_EQ(statisticsType.valueDataOptions->render, false);
+  EXPECT_EQ(statisticsType.valueDataOptions->scaleToBlockSize, false);
+  EXPECT_EQ(statisticsType.valueDataOptions->colorMapper,
+            color::ColorMapper({0, 255}, color::PredefinedType::Jet));
+
+  EXPECT_FALSE(statisticsType.vectorDataOptions);
+  EXPECT_EQ(statisticsType.gridOptions, StatisticsType::GridOptions());
+
+  EXPECT_FALSE(anyValueModified(statisticsType));
+}
+
+TEST(StatisticsTypePlaylistHandlerTest,
+     LoadStatisticsType_VectorDataOptionsInXML_ShouldBeCorrectlySetInStatisticsType)
+{
+  auto statisticsType = stats::StatisticsTypeBuilder(1, "SomeTestType").build();
+
+  const auto document = loadAndCheckXMLDocument(
+      "<playlistItemStatisticsFile>\n"
+      " <statType1 mapVectorToColor=\"1\" renderVectorData=\"0\" renderVectorDataValues=\"0\" "
+      "renderarrowHead=\"1\" scaleVectorToZoom=\"1\" vectorScale=\"4\" vectorStyle=\"#80164c 2 3\">"
+      "SomeTestType</statType1>\n"
+      "</playlistItemStatisticsFile>\n");
+
+  StatisticsTypePlaylistHandler::tryToLoadFromPlaylist(statisticsType, document.documentElement());
+
+  EXPECT_EQ(statisticsType.vectorDataOptions->render, false);
+  EXPECT_EQ(statisticsType.vectorDataOptions->renderDataValues, false);
+  EXPECT_EQ(statisticsType.vectorDataOptions->scaleToZoom, true);
+  EXPECT_EQ(statisticsType.vectorDataOptions->style,
+            LineDrawStyle({Color(128, 22, 76), 2.0, Pattern::DashDot}));
+  EXPECT_EQ(statisticsType.vectorDataOptions->scale, 4);
+  EXPECT_EQ(statisticsType.vectorDataOptions->mapToColor, true);
+  EXPECT_EQ(statisticsType.vectorDataOptions->arrowHead, StatisticsType::ArrowHead::circle);
+
+  EXPECT_FALSE(statisticsType.valueDataOptions);
+  EXPECT_EQ(statisticsType.gridOptions, StatisticsType::GridOptions());
+
+  EXPECT_FALSE(anyValueModified(statisticsType));
+}
+
+TEST(StatisticsTypePlaylistHandlerTest,
+     LoadStatisticsType_GridOptionsInXML_ShouldBeCorrectlySetInStatisticsType)
+{
+  auto statisticsType = stats::StatisticsTypeBuilder(1, "SomeTestType").build();
+
+  const auto document = loadAndCheckXMLDocument(
+      "<playlistItemStatisticsFile>\n"
+      " <statType1 gridStyle=\"#0c0ff0 1.25 1\" renderGrid=\"1\" scaleGridToZoom=\"1\">"
+      "SomeTestType</statType1>\n"
+      "</playlistItemStatisticsFile>\n");
+
+  StatisticsTypePlaylistHandler::tryToLoadFromPlaylist(statisticsType, document.documentElement());
+
+  EXPECT_EQ(statisticsType.gridOptions.render, true);
+  EXPECT_EQ(statisticsType.gridOptions.style,
+            LineDrawStyle({Color(12, 15, 240), 1.25, Pattern::Dash}));
+  EXPECT_EQ(statisticsType.gridOptions.scaleToZoom, true);
+
+  EXPECT_FALSE(statisticsType.valueDataOptions);
+  EXPECT_FALSE(statisticsType.vectorDataOptions);
+
+  EXPECT_FALSE(anyValueModified(statisticsType));
 }
 
 } // namespace stats::test
