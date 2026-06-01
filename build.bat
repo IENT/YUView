020273f69f7778fbfc2abc87a28f0ad81edad941
think3r <think3r@foxmail.com>
2026-06-01 17:57:17 +0800

    chore(): add windows build.bat

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\18\Insiders\VC\Auxiliary\Build\vcvars64.bat"
+if not exist build mkdir build
+cd build
+if not exist Makefile qmake CONFIG+=UNITTESTS ..
+jom
+pause
