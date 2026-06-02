8c6e100de267ae03e6e75bb963d6d4be00530700
think3r <think3r@foxmail.com>
2026-06-02 16:40:15 +0800

    chroe(): remove test form build.bat

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 call "C:\Program Files\Microsoft Visual Studio\18\Insiders\VC\Auxiliary\Build\vcvars64.bat"
 if not exist build mkdir build
 cd build
-if not exist Makefile qmake CONFIG+=UNITTESTS ..
+if not exist Makefile qmake ..
 jom
 pause
